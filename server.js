5f69357e8333037767334aae12236b2b3d8b1fce
yatendrakumar882-hue <yatendrakumar882@gmail.com>
2026-09-25 05:24:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -277,9 +277,9 @@ app.post('/api/send-stream', async (req, res) => {
       res.write(`data: ${JSON.stringify(failData)}\n\n`);
     }
 
-    // Exact 60 ms delay execution
+    // Exact 45 ms delay execution
     if (i < recipients.length - 1 && !globalSession.stopRequested) {
-      await new Promise(resolve => setTimeout(resolve, 60));
+      await new Promise(resolve => setTimeout(resolve, 45));
     }
   }
 
